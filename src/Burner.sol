76d208dd7e24a82fdcd94c151607f603dcfd73b5
high_byte <email@hanan.beer>
2024-11-14 11:33:08 +0200

    nicer logs

--- src/Burner.sol
+++ src/Burner.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.13;
 contract Burner {
     constructor(address target, bytes memory data) {
         assembly {
-            sstore(0xdeadbeef, address())
             pop(delegatecall(gas(), target, add(data, 0x20), mload(data), 0x00, 0x00))
             // optionally deploy the INVALID (0xfe) opcode instead of STOP (0x00)
             mstore(0x00, 0xfe)
--- test/Burner.t.sol
+++ test/Burner.t.sol
@@ -21,39 +21,80 @@ contract BurnerTest is Test {
         return abi.encodePacked(type(Burner).creationCode, abi.encode(callbackTarget, callbackData));
     }
 
-    function lightBurner(address deployer, uint256 salt, address callbackTarget, bytes memory callbackData) internal {
-        vm.startPrank(deployer);
-        bytes32 bytecodeHash = keccak256(getParameterizedCreationCode(callbackTarget, callbackData));
-        address calculatedBurnerAddress = calcCreate2Address(deployer, salt, bytecodeHash);
-        Burner burner = new Burner{salt: bytes32(salt)}(callbackTarget, callbackData);
-        require(address(burner) == calculatedBurnerAddress, "Burner address mismatch");
-        require(address(burner).code.length == 1, "Burner code length is not 1");
-        vm.stopPrank();
+    function _lightBurner(uint256 salt, bytes memory creationCode) internal returns (address burner) {
+        assembly {
+            burner := create2(0, add(creationCode, 0x20), mload(creationCode), salt)
+        }
+    }
 
-        console.log("burner deployed at:", address(burner));
+    function lightBurner(uint256 salt, bytes memory creationCode) internal returns (address) {
+        address deployer = address(this);
+        address burner = _lightBurner(salt, creationCode);
+
+        require(burner != address(0), "contract was already burned");
+
+        // verify burner deployed the expected bytecode, which is just the INVALID opcode
+        bytes memory code = burner.code;
+        require(code.length == 1, "burner code length is not 1");
+        require(code[0] == 0xfe, "burner opcode is expected to be 0xfe");
+        return burner;
     }
 
     function test_Burner() public {
-        // address deployer = address(this);
         address deployer = makeAddr("Create3");
-        console.log("burner deployer address:", deployer);
-
         address callbackTarget = makeAddr("Burner Callback Contract");
-        console.log("burner target address:", callbackTarget);
 
-        // ensure target has the callback
+        // for testing ensure target is a BurnerCallback
         vm.etch(callbackTarget, type(BurnerCallback).runtimeCode);
+        vm.store(callbackTarget, bytes32(uint256(0xdeadbeef)), bytes32(uint256(uint160(callbackTarget))));
+
+        uint256 param1 = 1;
+        uint256 param2 = 2;
 
         // encode burner params
         uint256 salt = 0xcafebabe;
-        bytes memory callbackData = abi.encodeWithSignature("onBurn(uint256,uint256)", 1, 2);
+        bytes memory callbackData = abi.encodeWithSignature("onBurn(uint256,uint256)", param1, param2);
+        bytes memory creationCode = getParameterizedCreationCode(callbackTarget, callbackData);
+        bytes32 bytecodeHash = keccak256(creationCode);
+        console.log("burner params:");
+        console.log("|- salt              = %x", salt);
+        console.log("|- bytecodeHash      = %x", uint256(bytecodeHash));
+        console.log("|- deployer          =", deployer);
+        console.log("|-----------------");
 
-        lightBurner(deployer, salt, callbackTarget, callbackData);
+        // verify burner deployed to expected address
+        address calculatedBurnerAddress = calcCreate2Address(deployer, salt, bytecodeHash);
+
+        console.log("|- callback address  =", callbackTarget);
+        console.log("|- callback data     = onBurn(uint256(%x), uint256(%x))", param1, param2);
+        // console.logBytes(callbackData);
+        console.log("\\_ burner contract   =", calculatedBurnerAddress);
+        console.log("");
+
+        vm.startPrank(deployer);
+
+        // burn it (ie. deploy)
+        address burner = lightBurner(salt, creationCode);
+
+        // verify burner deployed to the expected address        
+        require(burner == calculatedBurnerAddress, "burner address mismatch");
+
+        // lightBurner() will return 0 if the contract was already deployed
+        require(_lightBurner(salt, creationCode) == address(0), "doule burn should be impossible");
+        vm.stopPrank();
     }
 }
 
 contract BurnerCallback {
     function onBurn(uint256 param1, uint256 param2) external payable {
-        console.log("onBurn() called from %s with %s bytes of additional data", msg.sender, msg.data.length - 4);
+        console.log("onBurn() called:");
+        address codeAddr;
+        assembly {
+            codeAddr := sload(0xdeadbeef)
+        }
+        console.log("|- burner deployer   =", msg.sender);
+        console.log("|- burner contract   =", address(this));
+        // console.log("  burner callback   =", SELF); // set SELF = address(this) in constructor if you want to access it
+        console.log("\\_ data length       =", msg.data.length - 4);
     }
 }
